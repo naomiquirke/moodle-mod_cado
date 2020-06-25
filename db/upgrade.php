630f0b799f0906e18acdd19f462c997aa07e162f
Naomi Quirke <naomiquirke@gmail.com>
2020-06-25 18:00:11 +1200

    Turned upgrade.php into stub

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,11 +1,34 @@
 <?php
- 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version 1.1
+ * Database upgrade stub
+ *
+ * @package   mod_cado
+ * @copyright 2020 onwards  Naomi Quirke
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ /*
 function xmldb_cado_upgrade($oldversion) {
     global $DB;
     $dbman = $DB->get_manager();
     $result = TRUE;
  
-/*    if ($oldversion < 2020022100) {
+    if ($oldversion < 2020022100) {
 
         // Define field id to be added to cado.
         $table = new xmldb_table('cado');
@@ -20,7 +43,7 @@ function xmldb_cado_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2020022100, 'cado');
     }
 
-    */
+    
     return $result;
 }
-?>
+*/
