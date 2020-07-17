b015de65beb4d85f823883558535b0743300b39e
Naomi Quirke <naomiquirke@gmail.com>
2020-07-17 12:11:15 +1200

    ver

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2020071407;
+$plugin->version  = 2020071700;
 $plugin->requires = 2018051700; // Requires 3.5.
 $plugin->supported = [35, 39]; // range from 3.5 to 3.9.
 $plugin->component = 'mod_cado';
